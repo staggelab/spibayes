bd6080352dd9659b15063124e0b61d84b8a1861c
Jim Stagge <jhstagge@gmail.com>
2020-06-01 22:48:07 -0400

    Fixed the utility functions so they show up

--- R/utilities.R
+++ R/utilities.R
@@ -1,7 +1,15 @@
 
-###########################################################################
-###  Extract draws from a fitdistrplus distribution
-###########################################################################
+#' Extract draws from a fitdistrplus distribution
+#'
+#' This is where you describe the function itself
+#' contains the rownames and the subsequent columns are the sample identifiers.
+#' Any rows with duplicated row names will be dropped with the first one being
+#'
+#' @param data Dataframe with the underlying data. Columns must include variable names
+#' @param spline_type List of spline types. Either cc or cr are accepted. Names must agree with knot_loc
+#' @param knot_loc List of knot locations
+#' @return A matrix of the infile
+#' @export
 draw_fitgamma <- function(data, n){
 	data <- data[data > 0 & !is.na(data)]
 	fitted <- fitdist(data, "gamma")
@@ -24,10 +32,18 @@ draw_fitgamma <- function(data, n){
 	return(bind_rows(estimate, draws))
 }
 
-###########################################################################
-###  Confidence interval on fitdisrplus gamma
-###########################################################################
 
+#' Confidence interval on fitdisrplus gamma
+#'
+#' This is where you describe the function itself
+#' contains the rownames and the subsequent columns are the sample identifiers.
+#' Any rows with duplicated row names will be dropped with the first one being
+#'
+#' @param data Dataframe with the underlying data. Columns must include variable names
+#' @param spline_type List of spline types. Either cc or cr are accepted. Names must agree with knot_loc
+#' @param knot_loc List of knot locations
+#' @return A matrix of the infile
+#' @export
 mle_gamma_fit <- function(jdate, values, n, ci = 0.95){
 
 	interval <- (1-ci)/2
